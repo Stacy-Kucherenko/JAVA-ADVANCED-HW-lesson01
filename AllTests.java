e3c560bce8f5cf41298911211816d7f8928384b1
Stacy-Kucherenko <80332733+Stacy-Kucherenko@users.noreply.github.com>
2021-10-03 12:25:01 +0300

    Add files via upload

---
+++ AllTests.java
@@ -0,0 +1,10 @@
+package JAD_lesson1.JAD_lesson1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TimeTest.class, ScheduleTest.class })
+
+public class AllTests { }
---
+++ ScheduleTest.java
@@ -0,0 +1,51 @@
+package JAD_lesson1.JAD_lesson1;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+
+public class ScheduleTest {
+	
+	@Rule
+	public TestWatcher testWatcher = new TestWatcher() {
+		protected void succeeded(org.junit.runner.Description description) {
+			System.out.println("Executing " + description.getMethodName() + "... SUCCEED!");
+		};
+
+		protected void failed(Throwable exception, org.junit.runner.Description description) {
+			System.out.println("Executing " + description.getMethodName() + "... FAILED!");
+		};
+	};
+
+	Schedule schedule;
+	
+	@Before
+	public void beforeTest() throws IllegalTimeFormatException {
+		schedule = new Schedule();
+	}
+
+	@After
+	public void afterTest() {
+		schedule = null;
+	}
+
+	@Test
+	public void registerSeanceInScheduleTest() throws IllegalTimeFormatException {
+		schedule.registerSeanceInSchedule(new Seance(new Movie("Тест", new Time(2, 15)), new Time(10, 15)));		
+	}
+	
+	@Test
+	public void scheduleBusyTimeCheckTest() throws IllegalTimeFormatException {
+		schedule.registerSeanceInSchedule(new Seance(new Movie("Тест", new Time(2, 15)), new Time(10, 15)));
+		Assert.assertFalse(schedule.isSeanceTimeFree(new Seance(new Movie("Тест1", new Time(1, 45)), new Time(11, 00))));		
+	}
+	
+	@Test
+	public void scheduleFreeTimeCheckTest() throws IllegalTimeFormatException {
+		schedule.registerSeanceInSchedule(new Seance(new Movie("Тест", new Time(2, 15)), new Time(10, 15)));
+		Assert.assertTrue(schedule.isSeanceTimeFree(new Seance(new Movie("Тест1", new Time(1, 45)), new Time(13, 00))));		
+	}
+}
